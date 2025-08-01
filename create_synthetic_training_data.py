36325e34f774ea387f120b482e686b729c7ffd1b
Jonathan Häßler <jonathan.haessler@uni-konstanz.de>
2025-08-01 14:13:01 +0200

    Projection almost working

--- create_synthetic_training_data.py
+++ create_synthetic_training_data.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 import bpy
 import bmesh
@@ -7,18 +6,29 @@ import bpy_extras
 from mathutils import Matrix
 from mathutils import Vector
 
-from pathlib import Path
 import numpy as np
 import json
 import cv2
-    
+
+
+#---------------------------------------------------------------
+# Global variables
+#---------------------------------------------------------------
+
+
+keypoint_list:   list[str] = []
+collection_name: str       = ""
+object_name:     str       = ""
+use_cam_matrix:  bool      = True
+
 
 #---------------------------------------------------------------
 # 3x4 P matrix from Blender camera
+# !! With the unaltered code, x- and z-axis are swapped and the direction of the z-axis is inverted - also, the axes have a scale factor > 1 !!
 #---------------------------------------------------------------
 
 
-def get_calibration_matrix_K_from_blender(camd):
+def get_calibration_matrix_K_Blendercam2Blenderimage(camd):
     """
     Build intrinsic camera parameters from Blender camera data
 
@@ -34,12 +44,12 @@ def get_calibration_matrix_K_from_blender(camd):
     sensor_height_in_mm = camd.sensor_height
     pixel_aspect_ratio = scene.render.pixel_aspect_x / scene.render.pixel_aspect_y
     if (camd.sensor_fit == 'VERTICAL'):
-        # the sensor height is fixed (sensor fit is horizontal), 
+        # the sensor HEIGHT is FIXED (sensor fit is vertical), 
         # the sensor width is effectively changed with the pixel aspect ratio
         s_u = resolution_x_in_px * scale / sensor_width_in_mm / pixel_aspect_ratio 
         s_v = resolution_y_in_px * scale / sensor_height_in_mm
     else: # 'HORIZONTAL' and 'AUTO'
-        # the sensor width is fixed (sensor fit is horizontal), 
+        # the sensor WIDTH is FISXED (sensor fit is horizontal), 
         # the sensor height is effectively changed with the pixel aspect ratio
         pixel_aspect_ratio = scene.render.pixel_aspect_x / scene.render.pixel_aspect_y
         s_u = resolution_x_in_px * scale / sensor_width_in_mm
@@ -54,12 +64,13 @@ def get_calibration_matrix_K_from_blender(camd):
     skew = 0 # only use rectangular pixels
 
     K = Matrix(
-        ((alpha_u, skew,    u_0),
-        (    0  , alpha_v, v_0),
-        (    0  , 0,        1 )))
+        (  (alpha_u,  skew  , u_0),
+           (   0   , alpha_v, v_0),
+           (   0   ,    0   ,  1 )  ))
     return K
 
-def get_3x4_RT_matrix_from_blender(cam):
+
+def get_3x4_RT_matrix_Blender2Blendercam(cam):
     """
     Returns camera rotation and translation matrices from Blender.
 
@@ -77,45 +88,30 @@ def get_3x4_RT_matrix_from_blender(cam):
         - right-handed: positive z look-at direction
         bcam stands for blender camera
     """
-    R_bcam2cv = Matrix(
-        ((1, 0,  0),
-         (0, -1, 0),
-         (0, 0, -1)))
-
-    # Transpose since the rotation is object rotation, 
-    # and we want coordinate rotation
-    # R_world2bcam = cam.rotation_euler.to_matrix().transposed()
-    # T_world2bcam = -1*R_world2bcam * location
-    #
     # Use matrix_world instead to account for all constraints
     location, rotation = cam.matrix_world.decompose()[0:2]
     R_world2bcam = rotation.to_matrix().transposed()
 
-    # Convert camera location to translation vector used in coordinate changes
-    # T_world2bcam = -1*R_world2bcam*cam.location
     # Use location from matrix_world to account for constraints:     
     T_world2bcam = -1*R_world2bcam @ location
 
-    # Build the coordinate transform matrix from world to computer vision camera
-    # NOTE: Use * instead of @ here for older versions of Blender
-    # TODO: detect Blender version
-    R_world2cv = R_bcam2cv@R_world2bcam
-    T_world2cv = R_bcam2cv@T_world2bcam
-
     # put into 3x4 matrix
     RT = Matrix((
-        R_world2cv[0][:] + (T_world2cv[0],),
-        R_world2cv[1][:] + (T_world2cv[1],),
-        R_world2cv[2][:] + (T_world2cv[2],)
-         ))
-    return RT
+        R_world2bcam[0][:] + (T_world2bcam[0],),
+        R_world2bcam[1][:] + (T_world2bcam[1],),
+        R_world2bcam[2][:] + (T_world2bcam[2],)
+    ))
 
-def get_3x4_P_matrix_from_blender(cam):
-    K = get_calibration_matrix_K_from_blender(cam.data)
-    RT = get_3x4_RT_matrix_from_blender(cam)
-    return K@RT, K, RT
+    return RT, R_world2bcam, T_world2bcam
 
-# ----------------------------------------------------------
+
+def get_3x4_P_matrix_Blender2Blenderimage(cam):
+    K = get_calibration_matrix_K_Blendercam2Blenderimage(cam.data)
+    RT, R, T = get_3x4_RT_matrix_Blender2Blendercam(cam)
+    return K@RT, K, R, T, RT
+
+# -----------------------------
+
 def project_by_object_utils(cam, point):
     """        
     Alternate 3D coordinates to 2D pixel coordinate projection code
@@ -132,15 +128,62 @@ def project_by_object_utils(cam, point):
     return Vector((co_2d.x * render_size[0], render_size[1] - co_2d.y * render_size[1]))
 
 
+def blender2d_to_opencv2d(point2d):
+    """
+.    openCV: 
+.            
+.          .´ [Z camera: positive Z look-at]                                                                       .´ Z
+.        .´                                                                                                      .´    
+.        0/0---X--->                                                                                             0/0---X--->                  
+.        |                                                                                                       |                                   
+.        |    image:                                                                                             |     world:             
+.        Y    Point(x,y) -> column-major                                                                         Y     right-handed coordinate system, y pointing down
+.        |    Mat(y,x)   -> row-major                                                                            |     axes around positive x are clockwise Y, Z
+.        |                                                                                                       |     
+.        v                     ^    __                                                                           v        ^ 
+.                              |   |`.                                                                                    |
+.               (1,  0,  0)    |      `.                                                                                  |    (1,  0,  0)
+.               (0, -1,  0)----+        `.                                                                                +----(0,  0, -1)     = 90° ccw about x
+.               (0,  0, -1)    |          `.                   K @ Blcam2cvcam @ RT                                       |    (0,  1,  0)
+.               Blcam2cvcam    |            `.                                                                            |  Blworld2cvworld
+.                              |              `.___________________________________________________________________       |                                              
+.    Blender:                                                                                                      `.                    
+.                                                                                                                    `.                   
+.        ^                                                                                                       ^     world:
+.        |                                    <---------------------------.----------------.------------         |     right-handed coordinate system, z pointing up                     
+.        |    image:                                                    .´                  `.                   |     axes around positive x are clockwise Y, Z             
+.        Y    Vector(x,y) -> column-major                      (  a_w,  skew,  u_0)   ( Rxx,  Rxy,  Rxz,  tx)    Z
+.        |    Matrix(y,x) -> row-major                         (   0 ,   a_u,  v_0) @ ( Ryx,  Ryy,  Ryz,  ty)    |   .´ Y
+.        |                                                     (   0 ,    0 ,   1 )   ( Rzx,  Rzy,  Rzz,  tz)    | .´
+.        0/0---X--->                                                     K                      RT               0/0---X--->
+.      .´
+.    .´ [Z camera: negative Z look-at]   
+.          
+.            bpy_extras.object_utils.world_to_camera_view(scene, obj, coord)
+.            Returns the camera space coords for a 3d point. (also known as: normalized device coordinates - NDC).
+.            Where (0, 0) is the bottom left and (1, 1) is the top right of the camera frame. values outside 0-1 are also supported. 
+.            A negative 'z' value means the point is behind the camera.
+    """
+    scene = bpy.context.scene
+    render_scale = scene.render.resolution_percentage / 100
+    render_size = (
+        int(scene.render.resolution_x * render_scale),
+        int(scene.render.resolution_y * render_scale),
+    )
+    return (point2d[0], render_size[1] - point2d[1])
+
 
 #---------------------------------------------------------------
 # Keypoint and Mask Data Retrieval
 #---------------------------------------------------------------
 
+
 def get_deformed_mesh_data(collection_name, object_name):
     obj = bpy.data.collections[collection_name].objects[object_name]
     deps = bpy.context.evaluated_depsgraph_get()
     obj_eval = obj.evaluated_get(deps)
+
+    obj2world = obj_eval.matrix_world
 
     # create a mesh with modifiers, armature, shapekeys applied
     # -> docs: create a Mesh data-block from the current state of the object. The object owns the data-block. 
@@ -154,12 +197,6 @@ def get_deformed_mesh_data(collection_name, object_name):
     normals  = [tuple(v.normal)            for v in bm.verts]
     bm.free()
 
-    keypoint_list = [
-        'mouth tip', 'gill',
-        'root of pelvic fin','caudal peduncle',
-        'middle of caudal fin','lower tip of caudal fin'
-    ]
-
     # build a mapping group-name -> list of vertex indices in that group
     kpt2idx = {}
     for vg in obj_eval.vertex_groups:
@@ -171,9 +208,9 @@ def get_deformed_mesh_data(collection_name, object_name):
             ]
             kpt2idx[vg.name] = verts_in_group
 
-    # now get their coords:
-    kpt2verts_co = {
-        kpt: [tuple(mesh_eval.vertices[i].co) for i in idx_list]
+    # now get their coords in world space:
+    kpt2verts_worldco = {
+        kpt: [tuple(obj2world @ mesh_eval.vertices[i].co) for i in idx_list]
         for kpt, idx_list in kpt2idx.items()
     }
 
@@ -181,8 +218,40 @@ def get_deformed_mesh_data(collection_name, object_name):
     # docs: The object owns the mesh data-block. To force free it use to_mesh_clear(). 
     obj_eval.to_mesh_clear()
     
-    return faces, vertices, normals, kpt2verts_co
+    return faces, vertices, normals, kpt2verts_worldco
 
+
+def calculate_metadata(collection_name, object_name, cam_name):
+    faces, vertices, normals, kpt2verts_co = get_deformed_mesh_data(collection_name, object_name)
+
+    kpt2coords_world = get_avg_kpt_coords(kpt2verts_co)
+
+    obj_camera = bpy.data.objects.get(cam_name)
+    Blender_world_2_Blender_image, Blender_cam_2_Blender_image, R, T, Blender_world_2_Blender_cam = get_3x4_P_matrix_Blender2Blenderimage(obj_camera)
+
+    # invert y and z:
+    Blender_cam_2_cv_cam = Matrix(
+        (( 1,  0,  0),  # -> x becomes x
+         ( 0, -1,  0),  # -> y becomes negative y
+         ( 0,  0, -1))) # -> z becomes negative z
+    
+    Blender_world_2_cv_image = Blender_cam_2_Blender_image @ Blender_cam_2_cv_cam @ Blender_world_2_Blender_cam
+
+    kpt2coords_camera = {
+        kpt: Blender_world_2_cv_image@Vector(coords + (1,)) if use_cam_matrix else project_by_object_utils(obj_camera, Vector(coords))
+        for kpt, coords in kpt2coords_world.items()
+    }
+
+    kpt2coords_image = {
+        kpt: (coords_homog.x / coords_homog.z, coords_homog.y / coords_homog.z)
+            if use_cam_matrix
+            else (coords_homog.x, coords_homog.y)
+        for kpt, coords_homog in kpt2coords_camera.items()
+    }
+
+    return kpt2coords_image
+
+# -----------------------------
 
 def get_avg_kpt_coords(kpt2verts_co:dict):
     kpt2coords = {}
@@ -199,31 +268,6 @@ def get_avg_kpt_coords(kpt2verts_co:dict):
     return kpt2coords
 
 
-'''
-def create_discrete_color_map(kpt_names, cmap=cv2.COLORMAP_RAINBOW, RGB=False):
-    """
-    From a pre-defined cv2-colormap and a list of keypoints, assign equally distributed colors from cmap to each keypoint.
-    
-    Returns:
-        kpt_name_2_color (Dict[str, Tuple[int, int, int]]) containing the keypoint names as keys and a 3-tuple (0-255) RGB color as values.
-    """
-    n = len(kpt_names)
-    if n < 2:
-        raise ValueError("At least two keypoint names are required for color mapping.")
-
-    step_values = np.linspace(0, 255, n, dtype=np.uint8)
-    kpt_name_2_color = {}
-
-    for kpt, val in zip(kpt_names, step_values):
-        gray = np.array(np.uint8([[val]]))  # 1x1 grayscale image
-        color_bgr = cv2.applyColorMap(gray, cmap)[0, 0]
-        color_rgb = tuple(int(c) for c in color_bgr[::-1])  # Convert BGR to RGB
-        kpt_name_2_color[kpt] = [int(v) for v in color_rgb] if RGB else [int(v) for v in color_bgr]
-
-    return kpt_name_2_color
-'''
-
-
 def draw_kpts_on_img(kpt2coords, img_path, out_path, annot_radius = 5):
     """
     Args:
@@ -235,21 +279,11 @@ def draw_kpts_on_img(kpt2coords, img_path, out_path, annot_radius = 5):
 
     for name,(x,y) in kpt2coords.items():
         cv2.circle(img=img, center=(int(x),int(y)), radius=annot_radius, color=(255, 0, 0), lineType=-1)
-        cv2.putText(img, name, (int(x),int(y)), cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.3, color=(255, 0, 0), thickness=1)
+        cv2.putText(img, name, (int(x),int(y+annot_radius+10)), cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.3, color=(255, 0, 0), thickness=1)
 
     cv2.imwrite(str(out_path), img)
-    
-    
-def blender2d_to_opencv2d(point2d):
-    scene = bpy.context.scene
-    render_scale = scene.render.resolution_percentage / 100
-    render_size = (
-        int(scene.render.resolution_x * render_scale),
-        int(scene.render.resolution_y * render_scale),
-    )
-    return (point2d[0], render_size[1] - point2d[1])
 
-    
+
 def get_seg_mask_polygon(cam_matrix, faces):
     """
     get seg mask polygon
@@ -264,30 +298,10 @@ def get_seg_mask_polygon(cam_matrix, faces):
     """
 
 
-def calculate_metadata(collection_name, object_name):
-    faces, vertices, normals, kpt2verts_co = get_deformed_mesh_data(collection_name, object_name)
-
-    kpt2coords_world = get_avg_kpt_coords(kpt2verts_co)
-
-    obj_camera = bpy.context.scene.camera
-    P, K, RT = get_3x4_P_matrix_from_blender(obj_camera)
-
-    kpt2coords_camera = {
-        kpt: P@Vector(coords)
-        for kpt, coords in kpt2coords_world.items()
-    }
-    kpt2coords_image = {
-        kpt: blender2d_to_opencv2d((coords_homog.x / coords_homog.z, coords_homog.y / coords_homog.z))
-        for kpt, coords_homog in kpt2coords_camera.items()
-    }
-
-    return kpt2coords_image
-
-
-
 #---------------------------------------------------------------
 # Rendering
 #---------------------------------------------------------------
+
 
 def render_image_from_active_cam(out_path):
     bpy.context.scene.render.filepath = out_path
@@ -311,8 +325,6 @@ class TimedRender(bpy.types.Operator):
 
     OUT_DIR   = "//synthetic_data"
     ANNOT_DIR = "//synthetic_data" + os.sep + "annot"
-
-    last_metadata: dict | None = None
 
 
     def render_init(self, scene, depsgraph):
@@ -393,23 +405,6 @@ class TimedRender(bpy.types.Operator):
         context.window_manager.event_timer_remove(self.timer_event)
         bpy.types.RenderSettings.use_lock_interface = False
 
-    def annotate(self, metadata):
-        if metadata is not None:
-            try:
-                annot_filepath = bpy.path.abspath(self.ANNOT_DIR + os.sep + metadata["annot_filename"])
-                #print(f'Annotating image, reading from {metadata["path"]}, saving to {annot_filepath}')
-                if metadata["frame_index"] <= self.total + 1 - len(self.render_queue):
-                    draw_kpts_on_img(
-                        kpt2coords = metadata["kpt2coords"], 
-                        img_path   = metadata["rendered_image_path"], 
-                        out_path   = annot_filepath
-                    )
-            except:
-                self.report(
-                    {'ERROR_INVALID_INPUT'}, message=f"Error while annotating previous image: {metadata['rendered_image_path']}"
-                )
-                return {'CANCELLED'}
-
     def modal(self, context, event):
         # cancelling manually via pressing escape
         if event.type == 'ESC':
@@ -463,7 +458,7 @@ class TimedRender(bpy.types.Operator):
                     # annotate immediately
                     # try:
                     draw_kpts_on_img(
-                        kpt2coords = calculate_metadata("Bluegill","Body"),
+                        kpt2coords = calculate_metadata(collection_name, object_name, cam_name),
                         img_path   = bpy.path.abspath(img_file),
                         out_path   = os.path.join(bpy.path.abspath(self.ANNOT_DIR), render_basename + "_annot.png"),
                     )
@@ -473,11 +468,46 @@ class TimedRender(bpy.types.Operator):
 
         return {'PASS_THROUGH'}
 
-    
+
+#---------------------------------------------------------------
+# Execution context
+#---------------------------------------------------------------
+
 
 def unregister_timed_render():
     bpy.utils.unregister_class(TimedRender)
 
 if __name__ == "__main__":
+
+    test           = False
+    use_cam_matrix = True
+    ortho          = False
+
+
+    if test:
+        keypoint_list = [
+            'x0', 'y0', 'z0', 'x1', 'y1', 'z1', 'origin'
+        ]
+        collection_name = "Test"
+        object_name = "Cube"
+
+    else:
+        keypoint_list = [
+            'mouth tip', 'gill',
+            'root of pelvic fin','caudal peduncle',
+            'middle of caudal fin','lower tip of caudal fin'
+        ]
+        collection_name = "Bluegill"
+        object_name = "Body"
+
+
+    cam_objects = bpy.data.collections['Cameras'].objects
+    for cam_name in cam_objects.keys():
+        cam = bpy.data.objects.get(cam_name)
+        cam.data.type = 'ORTHO' if ortho else 'PERSP'
+        if ortho:
+            cam.data.ortho_scale = 30
+
+
     bpy.utils.register_class(TimedRender)
     bpy.ops.render.timed_render()
