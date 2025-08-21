cb4bf9c747d61b9625c5e905e14b1cc07c9c6813
Jonathan Häßler <jonathan.haessler@uni-konstanz.de>
2025-08-21 12:45:12 +0200

    directory structure + symlink changes

--- train_yolo.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO, checks, hub
-checks()
-
-hub.login('0284d16a9b4a61598d22c2ac30f0a18b1c62f5762e')
-
-model = YOLO('https://hub.ultralytics.com/models/nZIyzZFsMkkSq0uQHrMs')
-results = model.train()
